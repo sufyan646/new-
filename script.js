07dc89597c8f6ce73bd1f60d2f8720c0c970c903
sufyan646 <sufyant121@gmail.com>
2025-01-04 01:22:17 -0800

    start up

---
+++ script.js
@@ -0,0 +1,7 @@
+// Mobile Menu Toggle
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
